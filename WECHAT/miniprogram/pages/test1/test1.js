f5fca7deb30094159dd99f20b91fa4b3367f8dfa
padanes <45506046+padanes@users.noreply.github.com>
2018-12-04 11:16:16 +0800

    123

--- WECHAT/miniprogram/pages/test1/test1.js
+++ WECHAT/miniprogram/pages/test1/test1.js
@@ -11,30 +11,7 @@ Page({
   },
   //事件处理函数
   onLoad: function () {
-    wx.authorize({
-      scope: 'scope.userInfo',
-      success() {
-        wx.getUserInfo({
-          withCredentials: true,
-          lang: '',
-          success: function(res) {
-            this.setData({
-              userInfo: res.userInfo,
-              hasUserInfo: true
-            })
-          },
-        })
-      }
-    })
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
 
+  }
 
 })
