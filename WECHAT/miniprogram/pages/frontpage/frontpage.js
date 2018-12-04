a7d291fe767cd9abf9be5af1792f4286f050d4c5
padanes <45506046+padanes@users.noreply.github.com>
2018-12-04 20:23:21 +0800

    卢轶轩修改

--- WECHAT/miniprogram/pages/frontpage/frontpage.js
+++ WECHAT/miniprogram/pages/frontpage/frontpage.js
@@ -36,9 +36,9 @@ Page({
   },
 
   data: {
-    leftbutton: 1 ,
-    rightbutton: 0,
-    step:1
+    leftbutton: 1 , //左按钮指示 1：当前位于左按钮页面 0：不位于此页面
+    rightbutton: 0, //右按钮指示 1：当前位于右按钮页面 0：不位于此页面
+    step:1 //换页面指示 1：我的快件 2：个人中心
   },
 
   //刷新数据，访问数据库，寻找与本机_openid相同的订单，存储在queryResult数组内
@@ -59,6 +59,7 @@ Page({
     })
   },
 
+//按下左按钮会发生跳转和变色
   LEFT: function() {
     if(this.data.leftbutton == 0)
     {
@@ -74,11 +75,12 @@ Page({
         this.data.rightbutton = 0
       }
     }
+    //跳转至我的快件
     this.setData({
       step:1
     })
   },
-
+//按下右按钮会发生跳转和变色
   RIGHT: function () {
     if (this.data.rightbutton == 0) {
       this.setData({
@@ -92,16 +94,17 @@ Page({
         this.data.leftbutton = 0
       }
     }
+    //跳转至个人中心
     this.setData({
       step: 2
     })
   },  
 
+//按加号跳转至book页面
   changeToIndex: function() {
     wx.navigateTo({
       url: '../book/book'
     })
   }
-  
 
 })
