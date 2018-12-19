aa8a43d45aa39df34688c008c2445144f54b74e4
padanes <45506046+padanes@users.noreply.github.com>
2018-12-19 19:37:33 +0800

    新增云函数

--- WECHAT/miniprogram/pages/book/book.js
+++ WECHAT/miniprogram/pages/book/book.js
@@ -7,7 +7,10 @@ data: {
   phonenum:'',
   name: '',
   address: '',
-  state: 1
+  state: 2,
+  car: '',
+  box: '',
+  back: ''
   },
 //picker选择时间日期
 bindDateChange: function (e) {
@@ -52,7 +55,54 @@ userAddressInput: function (e) {
 //提交订单并跳转到首页
 clickMe: function (e) {
   const db = wx.cloud.database()
-    
+  wx.cloud.callFunction({
+    name:"distribute",
+    data:{
+      time: this.data.time,
+      postnum: this.data.userNum,
+      address: this.data.userAddress,
+    },
+    success: res => {
+      this.setData({
+        back: JSON.stringify(res.result)
+      })
+      console.log('调用云函数成功: ', res)
+      db.collection('orders').add({
+        data: {
+          date: this.data.date,
+          time: this.data.time,
+          postnum: this.data.userNum,
+          phonenum: this.data.userPhoneNum,
+          name: this.data.userName,
+          address: this.data.userAddress,
+          car: a,
+          box: b,
+          state: 1
+        },
+
+        success: res => {
+          // 在返回结果中会包含新创建的记录的 _id
+          wx.showToast({
+            title: '预约成功',
+          })
+          console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '预约失败'
+          })
+          console.error('[数据库] [新增记录] 失败：', err)
+        }
+
+      })
+
+    },
+    fail: err => {
+      console.error('调用云函数失败：', err)
+    }
+  })
+
   db.collection('orders').add({
     data: {
       date: this.data.date,
