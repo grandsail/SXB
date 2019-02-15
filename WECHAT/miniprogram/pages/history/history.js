6e8d69d387afff43149c90855ebf298f89f2e0e1
sxb666 <45492056+sxb666@users.noreply.github.com>
2019-02-15 22:39:06 +0800

    2.15

--- WECHAT/miniprogram/pages/history/history.js
+++ WECHAT/miniprogram/pages/history/history.js
@@ -55,48 +55,13 @@ Page({
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-    return {
-      title: 'input',
-      path: 'miniprogram/pages/test/test'
-    }
-  },
-
+  
   data: {
     focus: false,
     inputValue: '',
     queryResult: '',
     count: 0,
     id:''
-  },
-
-  onAdd: function (e) {
-    const db = wx.cloud.database()
-    const newCount = this.data.count + 1
-    db.collection('todos').add({
-      data: {
-        phoneNum: e.detail.value.phone,
-        count: newCount
-      },
-      success: res => {
-        // 在返回结果中会包含新创建的记录的 _id
-        wx.showToast({
-          title: '上传成功',
-        })
-        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '上传失败'
-        })
-        console.error('[数据库] [新增记录] 失败：', err)
-      }
-
-    })
   },
 
   onQuery: function () {
@@ -121,44 +86,6 @@ Page({
     })
   },
 
-  formSubmit: function (e) {
-    this.onAdd(e)
-    this.setData({
-      count:this.data.count + 1
-    })
-    this.onQuery()
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-  },
-  onRemove: function (e) {
-    let id = e.currentTarget.dataset.id
-       const db = wx.cloud.database()
-    db.collection('todos').doc(id).remove({
-         success: res => {
-           wx.showToast({
-             title: '删除成功',
-           })
-           this.setData({
-             counterId: '',
-             count: null,
-           })
-         },
-        fail: err => {
-           wx.showToast({
-             icon: 'none',
-             title: '删除失败',
-           })
-           console.error('[数据库] [删除记录] 失败：', err)
-         }
-       })
-     
-  },
-
-  formReset: function () {
-    this.setData ({
-      "queryResult":''
-    })
-    console.log('form发生了reset事件')
-  },
   detail: function (e) {
     let id = e.currentTarget.dataset.id
     wx.navigateTo({
